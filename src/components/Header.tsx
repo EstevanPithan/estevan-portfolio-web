e7d9304b599c1a70098accac0c29ae501bd626ab
EstevanPithan <estevanpithan@hotmail.com>
2025-08-16 19:38:47 -0300

    refactor(Header): remove unnecessary flex properties from header
    - Simplified header layout by removing redundant flex properties.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ export function Header() {
 	}
 
 	return (
-		<header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 flex w-full items-center justify-center border-b backdrop-blur">
+		<header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
 			<div className="container flex h-16 items-center justify-between">
 				<Link
 					to="/about"
