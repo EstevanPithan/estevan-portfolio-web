0c0bb6919d44b0afea4d50f04dc2ea70c1d2bc72
EstevanPithan <estevanpithan@hotmail.com>
2025-08-16 14:56:55 -0300

    feat(vite.config): enhance configuration with Tailwind CSS and server settings
    - Add Tailwind CSS plugin to the configuration
    - Set server port to 3000
    - Update plugins to include Tailwind CSS and React
    - Define path aliases for better module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	server: {
+		port: 3000,
+	},
+	plugins: [tailwindcss(), react()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src'),
+			'@pages': resolve(__dirname, 'src/pages'),
+			'@types': resolve(__dirname, 'src/types'),
+			'@contexts': resolve(__dirname, 'src/contexts'),
+		},
+	},
 })
